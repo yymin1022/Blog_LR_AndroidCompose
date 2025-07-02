bfca1330653a6d80cb616bca1e44363d52b97ac7
yymin1022 <yymin1022@gmail.com>
2025-07-02 23:05:50 +0900

    Update Project : Adjust Android Target&Compile SDK Version as 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.yong.blog"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.yong.blog"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
