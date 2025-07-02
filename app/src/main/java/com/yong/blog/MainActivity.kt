cf3197c49c6802085f32ba3ed44171c0ffd428f6
yymin1022 <yymin1022@gmail.com>
2025-07-02 23:44:59 +0900

    Update Project : Added MainActiivty Class as AndroidEntryPoint

--- app/src/main/java/com/yong/blog/MainActivity.kt
+++ app/src/main/java/com/yong/blog/MainActivity.kt
@@ -12,8 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yong.blog.ui.theme.Blog_LRTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
