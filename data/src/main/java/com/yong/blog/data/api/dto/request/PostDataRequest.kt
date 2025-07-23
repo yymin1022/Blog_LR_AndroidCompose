7ad70d8b13564db0b50ea1d2b69bdc2b16052077
yymin1022 <yymin1022@gmail.com>
2025-07-23 23:22:25 +0900

    Update data module : Improved Request DTO Structure

---
+++ data/src/main/java/com/yong/blog/data/api/dto/request/PostDataRequest.kt
@@ -0,0 +1,6 @@
+package com.yong.blog.data.api.dto.request
+
+data class PostDataRequest(
+    val postID: String,
+    val postType: String
+)
---
+++ data/src/main/java/com/yong/blog/data/api/dto/request/PostListRequest.kt
@@ -0,0 +1,5 @@
+package com.yong.blog.data.api.dto.request
+
+data class PostListRequest(
+    val postType: String
+)
