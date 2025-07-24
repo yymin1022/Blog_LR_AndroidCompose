2d278d9727d62d669622089a6489e6f2e3b1fe72
yymin1022 <yymin1022@gmail.com>
2025-07-24 23:31:23 +0900

    Update data module : Added API Manager Request / Response

--- data/src/main/java/com/yong/blog/data/api/manager/ApiManagerImpl.kt
+++ data/src/main/java/com/yong/blog/data/api/manager/ApiManagerImpl.kt
@@ -1,14 +1,21 @@
 package com.yong.blog.data.api.manager
 
+import com.yong.blog.data.api.dto.request.PostDataRequest
+import com.yong.blog.data.api.dto.request.PostImageRequest
+import com.yong.blog.data.api.dto.request.PostListRequest
+import com.yong.blog.data.api.service.ApiService
 import com.yong.blog.domain.model.PostData
 import com.yong.blog.domain.model.PostImage
 import com.yong.blog.domain.model.PostList
 
-class ApiManagerImpl: ApiManager {
+class ApiManagerImpl(private val api: ApiService): ApiManager {
     override suspend fun getPostData(
         postType: String,
         postID: String
     ): PostData {
+        val requestDto = PostDataRequest(postID, postType)
+        val responseDto = api.getPostData(requestDto)
+
         TODO("Not yet implemented")
     }
 
@@ -17,10 +24,16 @@ class ApiManagerImpl: ApiManager {
         postID: String,
         srcID: String
     ): PostImage {
+        val requestDto = PostImageRequest(postID, postType, srcID)
+        val responseDto = api.getPostImage(requestDto)
+
         TODO("Not yet implemented")
     }
 
     override suspend fun getPostList(postType: String): PostList {
+        val requestDto = PostListRequest(postType)
+        val responseDto = api.getPostList(requestDto)
+
         TODO("Not yet implemented")
     }
 }
