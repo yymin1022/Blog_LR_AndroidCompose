c77cd78ffd6f428618d753f2a43a9c09f2e1105f
yymin1022 <yymin1022@gmail.com>
2025-07-25 15:27:41 +0900

    Update data module : Changed PostListResponse Structure

--- data/src/main/java/com/yong/blog/data/api/dto/response/PostListResponse.kt
+++ data/src/main/java/com/yong/blog/data/api/dto/response/PostListResponse.kt
@@ -2,5 +2,14 @@ package com.yong.blog.data.api.dto.response
 
 data class PostListResponse(
     val postCount: Int,
-    val postList: List<PostDataResponse>
+    val postList: List<PostListItemResponse>
+)
+
+data class PostListItemResponse(
+    val postDate: String,
+    val postID: String,
+    val postIsPinned: String,
+    val postTag: List<String>,
+    val postTitle: String,
+    val postURL: String
 )
