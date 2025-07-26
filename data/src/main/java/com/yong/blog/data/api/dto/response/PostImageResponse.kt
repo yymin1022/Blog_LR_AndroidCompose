92c4a64ecaf5d82b376a97ec6bdbe90087201149
yymin1022 <yymin1022@gmail.com>
2025-07-26 15:28:49 +0900

    Update data module : Added toDomain() for PostImageResponse

--- data/src/main/java/com/yong/blog/data/api/dto/response/PostImageResponse.kt
+++ data/src/main/java/com/yong/blog/data/api/dto/response/PostImageResponse.kt
@@ -1,7 +1,13 @@
 package com.yong.blog.data.api.dto.response
 
 import com.google.gson.annotations.SerializedName
+import com.yong.blog.domain.model.PostImage
 
 data class PostImageResponse(
     @SerializedName("ImageData") val base64Str: String
 )
+
+fun PostImageResponse.toDomain() =
+    PostImage(
+        base64Str = this.base64Str
+    )
