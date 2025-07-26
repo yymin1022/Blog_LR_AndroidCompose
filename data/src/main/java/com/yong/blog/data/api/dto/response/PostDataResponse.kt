8f016cd505e66806693bc1d8568ade848aef9167
yymin1022 <yymin1022@gmail.com>
2025-07-26 15:28:40 +0900

    Update data module : Added toDomain() for PostDataResponse

--- data/src/main/java/com/yong/blog/data/api/dto/response/PostDataResponse.kt
+++ data/src/main/java/com/yong/blog/data/api/dto/response/PostDataResponse.kt
@@ -1,5 +1,7 @@
 package com.yong.blog.data.api.dto.response
 
+import com.yong.blog.domain.model.PostData
+
 data class PostDataResponse(
     val postContent: String,
     val postDate: String,
@@ -8,3 +10,14 @@ data class PostDataResponse(
     val postTitle: String,
     val postURL: String
 )
+
+fun PostDataResponse.toDomain(postID: String) =
+    PostData(
+        postContent = this.postContent,
+        postDate = this.postDate,
+        postID = postID,
+        postIsPinned = this.postIsPinned,
+        postTag = this.postTag,
+        postTitle = this.postTitle,
+        postUrl = this.postURL
+    )
