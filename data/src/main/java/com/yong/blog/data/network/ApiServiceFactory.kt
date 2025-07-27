8169110adcfa5724803c8fa9b0bedb7d529801fb
yymin1022 <yymin1022@gmail.com>
2025-07-27 18:14:05 +0900

    Update data module : Added ApiServiceFactory Implementation

--- data/src/main/java/com/yong/blog/data/network/ApiServiceFactory.kt
+++ data/src/main/java/com/yong/blog/data/network/ApiServiceFactory.kt
@@ -1,4 +1,23 @@
 package com.yong.blog.data.network
 
+import com.yong.blog.data.api.service.ApiService
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiServiceFactory {
+    private const val BLOG_API_URL = "https://api.dev-lr.com/"
+
+    fun createApiService(): ApiService {
+        val client = OkHttpClient.Builder()
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BLOG_API_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
 }
