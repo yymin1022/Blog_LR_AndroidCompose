2dcccaaed57b65e7fc6fb1991119823906f480d0
yymin1022 <yymin1022@gmail.com>
2025-07-27 23:53:52 +0900

    Added RepositoryModule Object

---
+++ app/src/main/java/com/yong/blog/di/RepositoryModule.kt
@@ -0,0 +1,35 @@
+package com.yong.blog.di
+
+import com.yong.blog.data.api.manager.ApiManager
+import com.yong.blog.data.api.manager.ApiManagerImpl
+import com.yong.blog.data.repository.PostDetailRepositoryImpl
+import com.yong.blog.data.repository.PostListRepositoryImpl
+import com.yong.blog.domain.repository.PostDetailRepository
+import com.yong.blog.domain.repository.PostListRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindApiManager(
+        impl: ApiManagerImpl
+    ): ApiManager
+
+    @Binds
+    @Singleton
+    abstract fun bindPostListRepository(
+        impl: PostListRepositoryImpl
+    ): PostListRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindPostDetailRepository(
+        impl: PostDetailRepositoryImpl
+    ): PostDetailRepository
+}
