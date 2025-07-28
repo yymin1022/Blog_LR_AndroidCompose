b4e85aad9bcc8c06c2de17de822d6b339f009e69
yymin1022 <yymin1022@gmail.com>
2025-07-28 23:17:01 +0900

    Update Detail ViewModel : Added Repository Fetching Logic

--- app/src/main/java/com/yong/blog/detail/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/yong/blog/detail/viewmodel/DetailViewModel.kt
@@ -1,4 +1,39 @@
 package com.yong.blog.detail.viewmodel
 
-class DetailViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yong.blog.domain.model.PostData
+import com.yong.blog.domain.repository.PostDetailRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val repository: PostDetailRepository
+): ViewModel() {
+    private val _postData = MutableStateFlow<PostData?>(null)
+    val postData: StateFlow<PostData?> = _postData.asStateFlow()
+
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
+
+    fun getPostData(type: String, id: String) {
+        viewModelScope.launch {
+            _isLoading.value = true
+
+            try {
+                val postDetail = repository.getPostData(type, id)
+                _postData.value = postDetail
+            } catch(e: Exception) {
+                // TODO: Error Handling
+                e.printStackTrace()
+            } finally {
+                _isLoading.value = false
+            }
+        }
+    }
 }
