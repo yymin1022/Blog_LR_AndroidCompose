72b9a7981cf6ecb22a488bc47a1cb1a841b3502f
yymin1022 <yymin1022@gmail.com>
2025-08-06 23:32:54 +0900

    Update MainActivity : Removed redundant Scaffold structure

--- app/src/main/java/com/yong/blog/activity/MainActivity.kt
+++ app/src/main/java/com/yong/blog/activity/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.yong.blog.navigation.BlogNavHost
@@ -22,12 +19,10 @@ class MainActivity: ComponentActivity() {
             val blogNavController = rememberNavController()
 
             Blog_LRTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    BlogNavHost(
-                        modifier = Modifier.padding(innerPadding),
-                        navController = blogNavController
-                    )
-                }
+                BlogNavHost(
+                    modifier = Modifier,
+                    navController = blogNavController
+                )
             }
         }
     }
