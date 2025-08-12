5a046fd5a7a6940938f4105c06cb51e8d4cbbf4a
yymin1022 <yymin1022@gmail.com>
2025-08-12 23:58:15 +0900

    Update List Screen : Cleaned UP

--- app/src/main/java/com/yong/blog/list/ui/ListScreen.kt
+++ app/src/main/java/com/yong/blog/list/ui/ListScreen.kt
@@ -1,9 +1,6 @@
 package com.yong.blog.list.ui
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
