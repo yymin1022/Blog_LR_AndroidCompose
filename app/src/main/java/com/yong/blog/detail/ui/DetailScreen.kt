4779e4e2aad801ce837f23506baeef80f53c182c
yymin1022 <yymin1022@gmail.com>
2025-08-23 23:59:37 +0900

    Update Detail Screen : Cleaned UP

--- app/src/main/java/com/yong/blog/detail/ui/DetailScreen.kt
+++ app/src/main/java/com/yong/blog/detail/ui/DetailScreen.kt
@@ -1,8 +1,6 @@
 package com.yong.blog.detail.ui
 
 import android.graphics.Bitmap
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
