079ab193859938e78545766ba911b1e4d5b43a22
yymin1022 <yymin1022@gmail.com>
2025-09-03 00:13:18 +0900

    Update MarkdownContent : Applied Markwon SyntaxHighlight w/Prism4j Plugin

--- app/src/main/java/com/yong/blog/detail/ui/MarkdownContent.kt
+++ app/src/main/java/com/yong/blog/detail/ui/MarkdownContent.kt
@@ -9,8 +9,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import ca.blarg.prism4j.languages.Prism4jGrammarLocator
 import io.noties.markwon.Markwon
 import io.noties.markwon.html.HtmlPlugin
+import io.noties.markwon.syntax.Prism4jThemeDarkula
+import io.noties.markwon.syntax.SyntaxHighlightPlugin
+import io.noties.prism4j.Prism4j
 
 @Composable
 fun MarkdownContent(
@@ -20,9 +24,12 @@ fun MarkdownContent(
     requestPostImage: (String) -> Unit
 ) {
     val context = LocalContext.current
+
+    val prism4j = remember { Prism4j(Prism4jGrammarLocator()) }
     val markwon = remember {
         Markwon.builder(context)
             .usePlugin(HtmlPlugin.create())
+            .usePlugin(SyntaxHighlightPlugin.create(prism4j, Prism4jThemeDarkula.create()))
             .build()
     }
 
