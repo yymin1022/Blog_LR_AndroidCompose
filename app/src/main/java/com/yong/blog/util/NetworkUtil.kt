671a096fcb171c062b869bc301f983200a9dbc71
yymin1022 <yymin1022@gmail.com>
2025-09-13 17:30:14 +0900

    Update NetworkUtil : Added Network Availability Method

--- app/src/main/java/com/yong/blog/util/NetworkUtil.kt
+++ app/src/main/java/com/yong/blog/util/NetworkUtil.kt
@@ -1,4 +1,23 @@
 package com.yong.blog.util
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
 object NetworkUtil {
+    var connManager: ConnectivityManager? = null
+
+    fun isNetworkAvail(context: Context): Boolean {
+        if(connManager == null) {
+            connManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        }
+
+        val activeNetwork = connManager?.activeNetwork ?: return false
+        val isActiveNetwork = connManager?.getNetworkCapabilities(activeNetwork) ?: return false
+        return when {
+            isActiveNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            isActiveNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            else -> false
+        }
+    }
 }
