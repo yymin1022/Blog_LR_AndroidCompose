c096b6406614cb47c9e719bff02e421243f17864
yymin1022 <yymin1022@gmail.com>
2025-09-20 18:17:43 +0900

    Update Data Module : Improved Post Image Exception

--- data/src/main/java/com/yong/blog/data/api/dto/response/ResponseWrapper.kt
+++ data/src/main/java/com/yong/blog/data/api/dto/response/ResponseWrapper.kt
@@ -4,6 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class ResponseWrapper<T>(
     @SerializedName("RESULT_CODE") val resultCode: Int,
-    @SerializedName("RESULT_MSG") val resultMessage: String,
+    @SerializedName("RESULT_MSG") val resultMessage: Any,
     @SerializedName("RESULT_DATA") val resultData: T?
 )
