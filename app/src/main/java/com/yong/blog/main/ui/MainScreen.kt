12f0908e9730d6c7339a620d3a06bb292f6bb143
yymin1022 <yymin1022@gmail.com>
2025-09-21 19:28:15 +0900

    Update MainScreen : Applied Main UI

--- app/src/main/java/com/yong/blog/main/ui/MainScreen.kt
+++ app/src/main/java/com/yong/blog/main/ui/MainScreen.kt
@@ -1,13 +1,18 @@
 package com.yong.blog.main.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.yong.blog.common.ui.BlogAppBar
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yong.blog.common.ui.theme.BlogBlue
 
 @Composable
 fun MainScreen(
@@ -16,14 +21,10 @@ fun MainScreen(
 ) {
     Scaffold(
         modifier = modifier,
-        topBar = {
-            BlogAppBar(
-                modifier = Modifier
-            )
-        }
     ) { innerPadding ->
         MainScreenBody(
             modifier = Modifier
+                .fillMaxSize()
                 .padding(innerPadding),
             navigateToList = navigateToList
         )
@@ -37,22 +38,78 @@ private fun MainScreenBody(
 ) {
     Column(
         modifier = modifier
+            .padding(horizontal = 20.dp, vertical = 40.dp)
     ) {
-        Text("Main")
-        Button(
+        MainTitle(
+            modifier = Modifier
+        )
+        PostListButton(
+            modifier = Modifier,
+            title = "Blog",
             onClick = { navigateToList("blog") }
-        ){
-            Text("Go to Blog List")
-        }
-        Button(
+        )
+        PostListButton(
+            modifier = Modifier,
+            title = "Project",
             onClick = { navigateToList("project") }
-        ){
-            Text("Go to Project List")
-        }
-        Button(
+        )
+        PostListButton(
+            modifier = Modifier,
+            title = "About",
             onClick = { navigateToList("about") }
-        ){
-            Text("Go to About List")
-        }
+        )
+    }
+}
+
+@Composable
+private fun MainTitle(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(bottom = 20.dp)
+    ) {
+        MainTitleText(
+            title = "안녕하세요"
+        )
+        MainTitleText(
+            title = "1인개발자",
+            titleColor = BlogBlue
+        )
+        MainTitleText(
+            title = "LR입니다"
+        )
+    }
+}
+
+@Composable
+private fun MainTitleText(
+    title: String,
+    titleColor: Color = Color.Black
+) {
+    Text(
+        text = title,
+        color = titleColor,
+        fontSize = 35.sp
+    )
+}
+
+@Composable
+private fun PostListButton(
+    modifier: Modifier = Modifier,
+    title: String,
+    onClick: () -> Unit
+) {
+    OutlinedButton(
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Text(
+            text = title,
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 20.sp
+            )
+        )
     }
 }
