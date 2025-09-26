8b035ed5bf6a35f036cc5c5ce9b35b7a78d5a0a5
yymin1022 <yymin1022@gmail.com>
2025-09-27 01:34:48 +0900

    Update Color : Added basic colors

--- app/src/main/java/com/yong/blog/common/ui/theme/Color.kt
+++ app/src/main/java/com/yong/blog/common/ui/theme/Color.kt
@@ -4,6 +4,13 @@ import androidx.compose.ui.graphics.Color
 
 val BlogBlue = Color(0xFF164EAB)
 
+val Black = Color(0xFF000000)
+val Gray10 = Color(0xFF1C1B1F)
+val Gray90 = Color(0xFFE6E1E5)
+
+val White = Color(0xFFFFFFFF)
+val White80 = Color(0xFFCAC4D0)
+
 val Blue80 = Color(0xFF164EAB)
 val Blue40 = Color(0xFF0F397F)
 
