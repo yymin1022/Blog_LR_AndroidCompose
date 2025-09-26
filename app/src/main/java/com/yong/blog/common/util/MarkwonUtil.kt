c53d6689602261e538bfbae1d88d8e2a13478053
yymin1022 <yymin1022@gmail.com>
2025-09-26 17:23:33 +0900

    Update Markwon Util : Added basic build function for markwon

--- app/src/main/java/com/yong/blog/common/util/MarkwonUtil.kt
+++ app/src/main/java/com/yong/blog/common/util/MarkwonUtil.kt
@@ -1,4 +1,12 @@
 package com.yong.blog.common.util
 
+import android.content.Context
+import io.noties.markwon.Markwon
+
 class MarkwonUtil {
+    fun createMarkwon(context: Context): Markwon {
+        val markwonBuilder = Markwon.builder(context)
+
+        return markwonBuilder.build()
+    }
 }
