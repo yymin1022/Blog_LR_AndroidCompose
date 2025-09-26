5c3277e8f7b96ecb1ae4df3aa02a3ed8777c81e1
yymin1022 <yymin1022@gmail.com>
2025-09-26 17:19:46 +0900

    Update MarkdownContent : Cleaned up

--- app/src/main/java/com/yong/blog/detail/ui/markdown/MarkdownContent.kt
+++ app/src/main/java/com/yong/blog/detail/ui/markdown/MarkdownContent.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
