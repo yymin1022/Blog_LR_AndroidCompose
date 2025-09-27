047a79cd22a2016377c5ba2a92d9d1c607a917d8
yymin1022 <yymin1022@gmail.com>
2025-09-27 16:54:57 +0900

    Update MarkdownContent : Applied color scheme theme

--- app/src/main/java/com/yong/blog/detail/ui/markdown/MarkdownContent.kt
+++ app/src/main/java/com/yong/blog/detail/ui/markdown/MarkdownContent.kt
@@ -9,12 +9,12 @@ import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -98,11 +98,12 @@ private fun MarkdownContentText(
     markwon: Markwon,
     markdownContent: String
 ) {
+    val textColor = MaterialTheme.colorScheme.onBackground
     AndroidView(
         modifier = modifier,
         factory = { ctx ->
             TextView(ctx).apply {
-                setTextColor(Color.Black.hashCode())
+                setTextColor(textColor.hashCode())
             }
         },
         update = {
