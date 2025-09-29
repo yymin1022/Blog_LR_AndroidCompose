e444e2c22db6fd863b8463b08812227331b953fd
yymin1022 <yymin1022@gmail.com>
2025-09-29 19:45:05 +0900

    Update BlogNavAnimation : Defined Enter Transition

--- app/src/main/java/com/yong/blog/navigation/BlogNavAnimation.kt
+++ app/src/main/java/com/yong/blog/navigation/BlogNavAnimation.kt
@@ -1,5 +1,19 @@
 package com.yong.blog.navigation
 
-object BlogNavAnimation {
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.core.tween
+import androidx.navigation.NavBackStackEntry
 
+private typealias NavEnterTransition = AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition
+
+object BlogNavAnimation {
+    private const val ANIMATION_DURATION_MS = 400
+
+    fun enterTransition(): NavEnterTransition = {
+        slideIntoContainer(
+            towards = AnimatedContentTransitionScope.SlideDirection.Left,
+            animationSpec = tween(durationMillis = ANIMATION_DURATION_MS)
+        )
+    }
 }
