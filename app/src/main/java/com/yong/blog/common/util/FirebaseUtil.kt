80d597f85f6d1a4969ee2148e8abb54f2cf13c8a
yymin1022 <yymin1022@gmail.com>
2025-09-30 21:33:15 +0900

    Update FirebaseUtil : Added event logginc method

--- app/src/main/java/com/yong/blog/common/util/FirebaseUtil.kt
+++ app/src/main/java/com/yong/blog/common/util/FirebaseUtil.kt
@@ -3,11 +3,34 @@ package com.yong.blog.common.util
 import com.google.firebase.Firebase
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.analytics
+import com.google.firebase.analytics.logEvent
 
 object FirebaseUtil {
     private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     fun init() {
         firebaseAnalytics = Firebase.analytics
+    }
+
+    fun logEvent(postType: String, postID: String? = null) {
+        if(postID == null) {
+            logListEvent(postType)
+        } else {
+            logDetailEvent(postType, postID)
+        }
+    }
+
+    private fun logListEvent(postType: String) {
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.VIEW_ITEM_LIST) {
+            param(FirebaseAnalytics.Param.ITEM_CATEGORY, postType)
+        }
+    }
+
+    private fun logDetailEvent(postType: String, postID: String) {
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM) {
+            param(FirebaseAnalytics.Param.ITEM_CATEGORY, postType)
+            param(FirebaseAnalytics.Param.ITEM_ID, postID)
+        }
+
     }
 }
