ee2b8c22e2c0e3e823d456016e4a03a8a7177c77
yymin1022 <yymin1022@gmail.com>
2025-09-30 21:32:26 +0900

    Update FirebaseUtil : Added analytics initialization

--- app/src/main/java/com/yong/blog/common/util/FirebaseUtil.kt
+++ app/src/main/java/com/yong/blog/common/util/FirebaseUtil.kt
@@ -1,4 +1,13 @@
 package com.yong.blog.common.util
 
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
+
 object FirebaseUtil {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+    fun init() {
+        firebaseAnalytics = Firebase.analytics
+    }
 }
