1ff72fe27cc296eaf21f1ed064b626ad2e0ca673
yymin1022 <yymin1022@gmail.com>
2025-10-02 17:59:53 +0900

    Update Project : Adjust Version Name / Version Code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.yong.blog"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
